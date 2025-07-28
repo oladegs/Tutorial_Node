a8c9798f4c249f4662180d05db5d4c2ad3f2983c
oladegs <adeyinkaoladega@gmail.com>
2025-07-27 20:12:21 -0400

    Jul-27-2025

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,4 +1,20 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-// OR : const app = require("express")();
+// Setup static and middleware
+app.use(express.static("./public"));
+
+// app.get("/", (req, res) => {
+// res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// adding to static assets
+// SSR - Server site rendering : We would use template engine for that
+// });
+
+// app.all("*", (req, res) => {
+//   res.status(404).send("Resource not found");
+// });
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000...");
+});
