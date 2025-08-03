cac374c0372bd92c425f00e29902e04ecd151968
oladegs <adeyinkaoladega@gmail.com>
2025-08-02 22:05:48 -0400

    Aug-02-2025

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -65,6 +65,27 @@ app.put("/api/people/:id", (req, res) => {
   res.status(200).json({ success: true, data: newPeople });
 });
 
+app.delete("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `Please person with id ${id}` });
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+
+  res.status(200).json({ success: true, data: newPeople });
+});
+
 app.listen(5000, () => {
-  console.log("Server is listening on port 5000..");
+  console.log("Server is listening on port 5000 !!!");
 });
